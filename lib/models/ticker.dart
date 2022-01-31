88204f10469c391c61b23ab517dfe3726127e45c
Mohamed Sadakathulla <sadakkathullah@gmail.com>
2022-02-01 04:01:12 +0530

    Delete ticker.dart

--- lib/models/ticker.dart
+++
@@ -1,9 +0,0 @@
-/// Object to increment time in seconds.
-class Ticker {
-  const Ticker();
-
-  /// Increments time by 1 second.
-  Stream<int> tick() {
-    return Stream.periodic(const Duration(seconds: 1), (x) => 1 + x++);
-  }
-}
